bd362a947176aba3c55997abdded162cb470f552
pw-02 <watters.patrick@outlook.com>
2025-03-20 16:34:48 -0700

    provided

--- deploy/deploy_function.go
+++ deploy/deploy_function.go
@@ -167,8 +167,8 @@ func createFunction(name string, svc *lambda.Lambda) {
 		Handler:      aws.String(options.LambdaKey),
 		MemorySize:   aws.Int64(options.MemorySize),
 		Role:         aws.String(options.Role),
-		Runtime:      aws.String("go1.x"),
-		//Runtime: aws.String("provided.al2023"),
+		//Runtime:      aws.String("go1.x"),
+		Runtime: aws.String("provided.al2023"),
 
 		Timeout:   aws.Int64(options.Timeout),
 		VpcConfig: vpcConfig,
